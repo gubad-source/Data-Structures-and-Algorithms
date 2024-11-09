cd0976465a010ecbe2e9d2aecb0bf5fe25768b4f
gubad-source <qubadmustafayev@gmail.com>
2024-11-09 17:59:07 +0400

    quicksort kind a

---
+++ Quicksort/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            //Console.WriteLine(arr.Length);//6
+            int n = arr.Length;
+
+            QuickSort(arr, 0, n - 1);
+            foreach (int val in arr)
+            {
+                Console.Write(val + " ");
+            }
+        }
+        static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            Swap(arr, i + 1, high);
+            return i + 1;
+        }
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        static void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+            }
+        }
+       
+    }
+}
